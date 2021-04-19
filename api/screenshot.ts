1f5151b0899bd2678790866a0c2099f1b131b071
RemiixInc <39117916+RemiixInc@users.noreply.github.com>
2021-04-19 17:02:40 +1000

    Update screenshot.ts

--- api/screenshot.ts
+++ api/screenshot.ts
@@ -1,5 +1,17 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 
 module.exports = (req: NowRequest, res: NowResponse) => {
-  res.status(200).send("hey")
+  try {
+    const file = await getScreenshot(req.query.url, req.query.width, req.query.height);
+    res.header('Content-Type', `image/png`);
+    res.header('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
+    res.status(200).end(file);
+  } catch (error) {
+    res.header('Content-Type', 'application/json');
+    res.json({
+      "success": false,
+      "error": "No url query",
+      "usage": "https://screenshot.totallyusefulapi.ml/api?url=https://totallyusefulapi.ml&width=1920&height=180"
+    });
+  }
 }
