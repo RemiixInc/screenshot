e0d4b54453f09d488e5376b6588742425f85f5e0
RemiixInc <39117916+RemiixInc@users.noreply.github.com>
2021-04-19 16:54:04 +1000

    Create chromium.ts

---
+++ api/_lib/chromium.ts
@@ -0,0 +1,21 @@
+import { launch, Page } from 'puppeteer-core';
+import { getOptions } from './options';
+let _page: Page | null;
+
+async function getPage() {
+    if (_page) {
+        return _page;
+    }
+    const options = await getOptions(false);
+    const browser = await launch(options);
+    _page = await browser.newPage();
+    return _page;
+}
+
+export async function getScreenshot(url, width, height) {
+    const page = await getPage();
+    await page.goto(url);
+    await page.setViewport({ width: width || 1920, height: height || 1080 });
+    const file = await page.screenshot({ 'png' });
+    return file;
+}
