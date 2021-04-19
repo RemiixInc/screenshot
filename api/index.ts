0610a790aa0d2df4cfe9d626e44612a5d18db7dd
RemiixInc <39117916+RemiixInc@users.noreply.github.com>
2021-04-19 17:09:36 +1000

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -4,11 +4,11 @@ import { getScreenshot } from './_lib/chromium';
 module.exports = async (req: NowRequest, res: NowResponse) => {
   try {
     const file = await getScreenshot(req.query.url, req.query.width, req.query.height);
-    res.header('Content-Type', `image/png`);
-    res.header('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
+    res.setHeader('Content-Type', `image/png`);
+    res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
     res.status(200).end(file);
   } catch (error) {
-    res.header('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'application/json');
     res.json({
       "success": false,
       "error": "No url query",
