55bc41315da7e1be2898465f220bf19dca64153c
Kieran <39117916+RemiixInc@users.noreply.github.com>
2022-08-13 14:48:19 +1000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
-import { Request, Response } from '@vercel/node'
-import { getScreenshot } from './_lib/puppeteer';
+const { getScreenshot } require('./_lib/puppeteer');
 const usage = "https://s.vercel.app/api?url=https://google.com&width=1280&height=720"
 
-module.exports = async (req: Request, res: Response) => {
+module.exports = async (req, res) => {
   if (!req.query.url) return res.status(400).send("No url query specified.");
   try {
     const file = await getScreenshot(req.query.url, req.query.width, req.query.height);
@@ -11,6 +10,6 @@ module.exports = async (req: Request, res: Response) => {
     res.status(200).end(file);
   } catch (error) {
     console.error(error)
-    res.status(400).send("The server encountered an error. You may have inputted an invalid query.");
+    res.status(500).send("The server encountered an error. You may have inputted an invalid query.");
   }
 }
