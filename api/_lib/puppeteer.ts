21912e44926f98624305f40a494ae4b676ba9512
Kieran <39117916+RemiixInc@users.noreply.github.com>
2023-07-09 17:20:36 +1000

    Update puppeteer.ts

--- api/_lib/puppeteer.ts
+++ api/_lib/puppeteer.ts
@@ -3,21 +3,21 @@ import chrome from "chrome-aws-lambda";
 let _page: Page | null;
 
 async function getPage() {
-    if (_page) return _page;
-    const options = { 
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless
-    };
-    const browser = await launch(options);
-    _page = await browser.newPage();
-    return _page;
+  if (_page) return _page;
+  const options = {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  };
+  const browser = await launch(options);
+  _page = await browser.newPage();
+  return _page;
 }
 
 export async function getScreenshot(url, width, height) {
-    const page = await getPage();
-    await page.goto(url);
-    await page.setViewport({ width: Number(width) || 1280, height: Number(height) || 720, deviceScaleFactor: 2 });
-    const file = await page.screenshot();
-    return file;
+  const page = await getPage();
+  await page.goto(url);
+  await page.setViewport({ width: Number(width) || 1280, height: Number(height) || 720, deviceScaleFactor: 2 });
+  const file = await page.screenshot();
+  return file;
 }
